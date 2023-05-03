254a8933f61a6718acc8982de9d15b721255844c
joelgeorge-web <joelgeorge98765@gmail.com>
2023-05-03 20:24:14 +0530

    gui changes

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,17 +1,36 @@
 import tkinter
-import customtkinter
+import customtkinter as ctk
 
-customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
-customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+ctk.set_appearance_mode("System")  # Modes: system (default), light, dark
+ctk.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
-app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+app = ctk.CTk()  # create CTk window like you do with the Tk window
 app.geometry("800x450")
+app.title("Kitchen Management")
 
 def button_function():
     print("Menu Edited")
+    window1 = ctk.CTkToplevel(app)
+    window1.geometry("800x450")
+    window1.title("Edit Functions")
+    button = ctk.CTkButton(master=app, text="Edit Menu", command=button_function)
+    button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+    app.mainloop()
 
-# Use CTkButton instead of tkinter Button
-button = customtkinter.CTkButton(master=app, text="Edit Menu", command=button_function)
+
+    
+button = ctk.CTkButton(master=app, text="Edit", command=button_function)
 button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 app.mainloop()
+
+
+
+
+
+
+
+
+
+
+
 
