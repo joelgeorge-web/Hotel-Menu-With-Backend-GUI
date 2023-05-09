7f8561e15bffe5614cca6dca624aeaa7cb455ae7
joelgeorge-web <joelgeorge98765@gmail.com>
2023-05-09 20:29:36 +0530

    auto open browser and gui with threads

--- Website/app.py
+++ Website/app.py
@@ -342,5 +342,5 @@ def open_browser():
     webbrowser.open_new('http://127.0.0.1:5000/')
 
 if __name__ == '__main__':
-    # threading.Timer(1.25, open_browser).start()
+    threading.Timer(1.25, open_browser).start()
     app.run()    
